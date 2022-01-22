25f6c863c3a0e4cfe7132a844085614a411754f5
Erik Nagel <erik@reallyhuman.net>
2022-01-22 14:49:16 +0100

    Initial commit.

---
+++ Vishnu.bin/Vishnu_Demo.bat
@@ -0,0 +1,4 @@
+REM Vishnu_Demo.bat
+REM Startet Vishnu mit einem Demo-Job.
+cd %Vishnu_Root%/ReadyBin/Vishnu.bin
+.\Vishnu.exe -UserAssemblyDirectory="../UserAssemblies" -Job="DemoJobs/Simple/CheckAll"
